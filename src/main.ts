708d99fd491361dfd990395e66f98b74aed24c29
Tlaster <tlaster@outlook.com>
2022-12-21 01:53:00 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,45 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token: string = core.getInput('github_token')
+    const octokit = github.getOctokit(token)
+    const readyLabel = core.getInput('ready_label')
+    const approvedLabel = core.getInput('approved_label')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // get all pull requests for the repo
+    const { data: pullRequests } = await octokit.rest.pulls.list({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      state: 'open',
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    // check if the pull request have the approved and ready labels
+    const pullRequestsToMerge = pullRequests.filter((pullRequest) => {
+      const labels = pullRequest.labels.map((label) => label.name)
+      return labels.includes(readyLabel) && labels.includes(approvedLabel)
+    })
+
+    // execute merge for each pull request
+    for (const pullRequest of pullRequestsToMerge) {
+      // check if the pull request is mergeable
+      const { data: pullRequestDetails } = await octokit.rest.pulls.get({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pullRequest.number,
+      })
+
+      if (pullRequestDetails.mergeable) {
+        // merge the pull request
+        await octokit.rest.pulls.merge({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          pull_number: pullRequest.number,
+        })
+      }
+    }
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
